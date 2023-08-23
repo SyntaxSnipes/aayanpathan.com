8661367430506d98497061262e1b287f4a90ae51
Mohammed Aayan Pathan <mohammedaayan@icloud.com>
2023-08-23 20:01:18 +0400

    Added 2 new sections and changed all sections. Added more fluid animations, and patched potential bugs and issues. -NOTE, BUTTON FUNCTIONALITY WILL BE IMPLEMENTED LATER-

---
+++ index.js
@@ -0,0 +1,27 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const slidingObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slide');
+        } else {
+            entry.target.classList.remove('slide');
+        }
+    });
+});
+
+const freezeElements = document.querySelectorAll('.freeze');
+freezeElements.forEach((el) => slidingObserver.observe(el));
